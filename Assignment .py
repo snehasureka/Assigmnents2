9d4afe13a977e584b47afaa25fe2840449444422
snehasureka <106825108+snehasureka@users.noreply.github.com>
2022-06-03 23:30:39 +0530

    Add files via upload

---
+++ Assignment .py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+d1 = ('31/05/2022')
+print(d1)
+
+
+# In[11]:
+
+
+t1 = ('12:20am')
+print(t1)
+
+
+# In[23]:
+
+
+a = [3,5,7,23]
+print(a)
+type(a)
+
+
+# In[24]:
+
+
+t = (3,5,7,23)
+print (t)
+type (t)
+
+
+# In[28]:
+
+
+color_list = ["Red","Green","White","Black"]
+print (color_list [0] , color_list[-1])
+
+
+# In[30]:
+
+
+for i in range (2,40,2):
+    print(i)
+
+
+# In[5]:
+
+
+n= [[1,2,3],[4,5,6],[10,11,12],[7,8,9]]
+print (max(n,key=sum))
+
+
+# In[11]:
+
+
+c= ['red','green','blue','white','black','orange']
+c.pop()
+
+
+# In[7]:
+
+
+c
+
+
+# In[8]:
+
+
+c.pop(3)
+
+
+# In[9]:
+
+
+c
+
+
+# In[14]:
+
+
+d= {'value1':5,'value2':10,'value3':5}
+# create a value to store result 
+answer =1
+
+#run a loop
+for i in d:
+    answer = answer*d[i]
+# print answer
+print(answer)
+
+
+# In[26]:
+
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4 = {1:10,2:20,3:30,4:40,5:50,6:60}
+print(dic4)
+
+
+# In[30]:
+
+
+c= ['red','green','blue','white','black','orange']
+c.clear ()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
